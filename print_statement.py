add2aedcb2d172c92957931f22012fd1aec08b0b
T. Sirisha <reddysirisa2000@gmail.com>
2024-08-16 11:41:54 +0530

    print_statement

---
+++ print_statement.py
@@ -0,0 +1,26 @@
+
+print("Hello World")
+
+myname = "Sirisha"
+
+print(f"Hello {myname}! How are you?")
+
+def hello_name(somename):
+    print(f"Hello {somename}! This is a function. ")
+
+
+def repeat_hello(somename, n_times):
+    for i in range(n_times):
+        print(f"Hello there {somename}! This is print statement number: {i+1}")
+
+
+if __name__ == "__main__":
+    hello_name("Class")
+    hello_name("Abhi")
+    hello_name("Nav")
+
+    repeat_hello( somename= "Abhi", n_times= 5)
+
+
+
+
