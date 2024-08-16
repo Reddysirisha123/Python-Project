37de1ca87799024b628fd53b916fea0811b95a3b
T. Sirisha <reddysirisa2000@gmail.com>
2024-08-16 13:21:21 +0530

    working files

---
+++ list_of_primes_up_to_n.py
@@ -0,0 +1,18 @@
+from prime_numbers import is_prime
+
+
+def list_of_primes_up_to_n(num):
+    primes_up_to_n = []
+    for i in range(1,num+1):
+        if is_prime(i):
+            primes_up_to_n.append(i)
+    return primes_up_to_n
+
+
+if __name__ == "__main__":
+    n = 100
+    primes = list_of_primes_up_to_n(n)
+    print(f"primes up to {n} = {primes}")
+
+
+
